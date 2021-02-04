870234a5702ffe93f3d0da066b7ae7592a9b993a
Natalia <nataliostapiuk@gmail.com>
2021-02-05 00:52:19 +0200

    h

---
+++ src/components/ErrorBoundary.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+  hasError: false
+    }
+  }
+componentDidCatch(erroe, info) {
+    this.setState({ hasError: true})
+}
+
+  render() {
+   if(this.state.hasError){
+       return(
+           <div>O nooo!</div>
+       )
+   }
+   return this.props.children
+  }
+}
+
+export default ErrorBoundary;
